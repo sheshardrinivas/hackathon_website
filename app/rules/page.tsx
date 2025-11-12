ce64796942a4a9d949436dfee962efe92ee43919
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2025-11-12 18:42:17 +0530

    added gsc

---
+++ app/rules/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div >
+      <h1>Welcome to the Registration Page</h1>
+      <p>Please fill out the form below to register.</p>
+
+    </div >
+  );
+} 
